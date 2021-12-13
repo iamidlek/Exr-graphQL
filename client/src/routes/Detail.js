5e4b99b79cdf690a2d7929bcdb78432bcef1ba4c
iamidlek <iamidlek@naver.com>
2021-12-13 14:19:57 +0900

    영화 목록 및 디테일 요청

--- client/src/routes/Detail.js
+++ client/src/routes/Detail.js
@@ -1,5 +1,28 @@
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
 const Detail = () => {
-  return <div>Detail</div>;
+  const { id } = useParams();
+  const { loading, data } = useQuery(GET_MOVIE, {
+    variables: { id: Number(id) },
+  });
+  if (loading) {
+    return "loading";
+  }
+  if (data && data.movie) {
+    return data.movie.title;
+  }
 };
 
 export default Detail;
